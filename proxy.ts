aeb07b281bb977c8ee76e557b58b73962d33b48f
Myo Min -7 <myomin-7@ms365primium.onmicrosoft.com>
2026-06-20 22:19:10 +0630

    finaldddddd

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,13 @@
-// middleware.ts
+// proxy.ts (middleware.ts ကနေ rename လုပ်ပါ)
 import { NextRequest, NextResponse } from "next/server";
 
+export default function proxy(request: NextRequest) {
+  const sessionCookie = request.cookies.get("better-auth.session_token");
 
-export async function proxy(request: NextRequest) {
-  const sessionCookie = request.cookies.get("better-auth.session_token"); // သင့် cookie name ကို သေချာစစ်ပါ
-
-  // Login မဝင်ထားရင် auth page တွေကလွဲရင် အကုန် Redirect လုပ်မယ်
   if (!sessionCookie && request.nextUrl.pathname.startsWith("/profile")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-  
+
   return NextResponse.next();
 }
 
