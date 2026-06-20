cfaa60aa4acd0431284afe8a51ed3a2feb2d8a0b
Myo Min -7 <myomin-7@ms365primium.onmicrosoft.com>
2026-06-20 21:36:24 +0630

    finalddddd

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,8 @@
-
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000"
+  // Vercel ကပေးတဲ့ URL ကို Environment Variable ထဲမှာ ထည့်ထားရပါမယ်
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000"
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
